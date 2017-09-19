ea3a6918d1eb9178f2bb7d6be1a5d25090c2e73e
diegocantesano <diegocantesano@gmail.com>
2017-09-19 20:48:04 -0300

    Nivel con objetivos
    3 objetivos simples
    1. pisar un boton para abrir un camino.
    2. subir asta llegar a un boton para abrir un camino
    3. responder una pregunta correcta

---
+++ Bottomless Pit/Assets/Juego/Scripts/Scripts puzz/Boton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour {
+
+	private GameObject boton;
+	public GameObject Borrar;
+	public GameObject Borrar2;
+	public GameObject Borrar3;
+	void Start () {
+		bool boton = false;
+	}
+	
+	void OnTriggerEnter (Collider otro) {
+		bool boton = true;
+		if (boton == true)
+			Destroy(Borrar);
+			Destroy(Borrar2);
+		Destroy(Borrar3);
+
+	}
+}
---
+++ Bottomless Pit/Assets/MovimientoPiso.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPiso : MonoBehaviour {
+
+	public Transform movimientoplataforma;
+	public Transform posicion1;
+	public Transform posicion2;
+	public Vector3 NuevaPosicion;
+	public string PosicionActual;
+	public float smooth;
+	public float reset;
+
+	void Start () {
+		CambiarObjetivo ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		movimientoplataforma.position = Vector3.Lerp (movimientoplataforma.position, NuevaPosicion, smooth * Time.deltaTime);
+	}
+
+	void CambiarObjetivo(){
+		if (PosicionActual == "posicion1")
+		{
+			PosicionActual = "posicion2";
+			NuevaPosicion = posicion2.position;
+		}else if(PosicionActual == "posicion2"){
+			PosicionActual = "posicion1";
+			NuevaPosicion = posicion1.position;
+		}else if(PosicionActual == ""){
+			PosicionActual = "posicion2";
+			NuevaPosicion = posicion2.position;
+		}
+		Invoke ("CambiarObjetivo", reset);
+	}	}
