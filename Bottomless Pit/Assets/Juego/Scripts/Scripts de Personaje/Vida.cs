c151234607b64583d24eb92aa3ec76e756a6b5a2
LuzGarcia97 <31461472+LuzGarcia97@users.noreply.github.com>
2017-09-19 23:58:42 -0300

    Primeros comportamientos del enemigo
    El enemigo persigue y mata al jugador, su velocidad es poca para permitir el testeo del resto del mapa

---
+++ Bottomless Pit/Assets/Juego/Scripts/Scripts de Personaje/Vida.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour 
+{
+ public float cantidadVida;
+
+	void Update()
+	{
+		if(cantidadVida <= 0)
+		{
+			Destroy (gameObject);
+		}
+	}
+}
