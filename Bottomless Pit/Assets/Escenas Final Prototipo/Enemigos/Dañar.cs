cbd562961ea83085a9ce2e2973ea31d273e5ebf4
Dreikenn <31456192+Dreikenn@users.noreply.github.com>
2017-09-22 01:09:56 -0300

    Corazones de vida
    chicos se esta llendo a la mierda el tema del orden despues en la facu de ultima lo ordenamos todo, hay q preguntarle al profe como hacerlo sin que se muera todo

---
+++ Bottomless Pit/Assets/Escenas Final Prototipo/Enemigos/Dañar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dañar : MonoBehaviour {
+    public float DMG = 1;
+
+    private GameObject BarraDeCorazones;
+
+    void Start()
+    {
+        BarraDeCorazones = GameObject.Find("Ilustrador");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        
+        if (col.tag == "Player")
+        {
+
+            BarraDeCorazones.SendMessage("daño", 1);
+           
+           
+        }
+    }
+
+   
+}
---
+++ Bottomless Pit/Assets/Escenas Final Prototipo/Vida/Corazones.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Corazones : MonoBehaviour {
+
+    public float CantidadDeVida;
+    public Image corazon;
+    
+    
+	
+	void Start () {
+
+        DibujarVida(CantidadDeVida);
+
+      
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+    }
+
+    void DibujarVida(float cantidad)
+    {
+        Image C;
+        for(int i=0; i< cantidad; i++)
+        {
+            C = Instantiate<Image>(corazon);
+            C.transform.SetParent(this.transform, false);
+            C.transform.Translate(60 * i, 0, 0);
+            C.name = "Heart" + i;
+        }
+    }
+
+    void DesaparecerVida(float num, bool estado)
+    {
+        GameObject[] hearts;
+        hearts = GameObject.FindGameObjectsWithTag("Corazon");
+        for (int i=0; i<hearts.Length; i++)
+        {
+            if (hearts[i].name == "Heart" + num)hearts[i].SetActive(estado);
+        }
+     
+    }
+
+    public void daño(float daño)
+    {
+        CantidadDeVida -= daño;
+        DesaparecerVida(CantidadDeVida, false);
+
+        if (CantidadDeVida <= 0)
+        {
+
+            Application.LoadLevel("Muerto");
+        }
+    }
+
+
+
+}
