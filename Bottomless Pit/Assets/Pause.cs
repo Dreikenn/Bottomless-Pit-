90a05460a1153977783620b0ea150b54dc0e468a
LuzGarcia97 <31461472+LuzGarcia97@users.noreply.github.com>
2017-09-24 03:25:53 -0300

    Pantalla de pause
    Funciona en cada nivel, no se ve que se para el tiempo por que la imagen es solida. Pero si se pone en pause.

---
+++ Bottomless Pit/Assets/Pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	public bool pausado;
+	public GameObject Pantalla;
+	// Use this for initialization
+	void Start () 
+	{
+		Pantalla.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			if (pausado == false) {
+				pausado = true;
+				Pantalla.SetActive (true);
+				Time.timeScale = 0;
+			}
+			else 
+			{
+				pausado = false;
+				Pantalla.SetActive (false);
+				Time.timeScale = 1;
+			}
+		}
+
+	}
+}
