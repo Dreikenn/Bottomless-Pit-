a8e0849dbde10ca7ee763f9003d57392bf4a141a
LuzGarcia97 <31461472+LuzGarcia97@users.noreply.github.com>
2017-10-15 18:19:45 -0300

    Barra
    Todavia tengo que ver como hacer que deje de decrecer

---
+++ Bottomless Pit/Assets/Escenas Final Prototipo/scripts/Scripts de Personaje/linterna/BarraEnergia.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarraEnergia : MonoBehaviour {
+ public float EnergiaMaxima = 100f;
+ public float EnergiaActual = 0f;
+ public GameObject Barra;
+	// Use this for initialization
+	void Start ()
+	{
+		EnergiaActual = EnergiaMaxima;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{ 
+		if (Input.GetKeyDown ("e"))
+		{
+			InvokeRepeating ("Decrecer", 1f, 1f);
+		}
+		
+	}
+
+	void Decrecer()
+	{
+		EnergiaActual -=10f;
+		float CalcularEnergia = EnergiaActual / EnergiaMaxima;
+
+		ManejarLaBarra (CalcularEnergia);
+	}
+
+	public void ManejarLaBarra(float BarraActual)
+	{
+		Barra.transform.localScale = new Vector3 (BarraActual, Barra.transform.localScale.y, Barra.transform.localScale.z);
+	}
+}
