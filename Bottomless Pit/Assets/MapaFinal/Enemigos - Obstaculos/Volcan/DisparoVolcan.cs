d7a7059667bd3722f05b705a84af2e43dbe6feb5
diegocantesano <diegocantesano@gmail.com>
2017-10-24 23:03:00 -0300

    Mapa enemigos y otras cosas

---
+++ Bottomless Pit/Assets/MapaFinal/Enemigos - Obstaculos/Volcan/DisparoVolcan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoVolcan : MonoBehaviour {
+
+		public GameObject prefab;
+		public float retrasoInicial;
+		public float retrasoDisparos;
+
+		void Awake()
+		{
+			InvokeRepeating ("Disparar", retrasoInicial, retrasoDisparos);
+		}
+
+		void Disparar()
+		{
+			Instantiate(prefab, transform.position, transform.rotation);
+		}
+	}
+
---
+++ Bottomless Pit/Assets/MapaFinal/Enemigos - Obstaculos/Volcan/MovimientoFuego.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoFuego : MonoBehaviour {
+
+	public float MovBala;
+
+	void Update()
+	{  
+		transform.Translate (0, MovBala, 0);
+	}
+	void OnTriggerEnter(){
+
+		Destroy(gameObject);
+	}
+}
