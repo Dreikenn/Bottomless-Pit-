7a1ec4536e4ca13329aa764c876a8e80de5b719c
LuzGarcia97 <31461472+LuzGarcia97@users.noreply.github.com>
2017-10-30 21:40:01 -0300

    Bengala
    Funciona apretando el Espacio

---
+++ Bottomless Pit/Assets/Bengalas/ScriptBengala/DispararBengala.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispararBengala : MonoBehaviour {
+
+
+	public GameObject Bengala;
+	public Transform reset;
+	public float cadena;
+	private float siguiente;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Space) && Time.time>siguiente)
+		{
+			Instantiate (Bengala, reset.position, reset.rotation);
+			siguiente = Time.time + cadena;
+
+		}
+	}
+
+}
---
+++ Bottomless Pit/Assets/Bengalas/ScriptBengala/MoverBengala.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverBengala : MonoBehaviour {
+
+	public float TransladarBengala;
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0, TransladarBengala);
+	}
+}
