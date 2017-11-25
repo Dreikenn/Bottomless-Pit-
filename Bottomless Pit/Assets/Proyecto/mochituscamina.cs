6fd284ed6e398c577be8501a533da59e3267866a
fedejavitaroco <31699354+fedejavitaroco@users.noreply.github.com>
2017-11-24 23:35:08 -0300

    asdas

---
+++ Bottomless Pit/Assets/Proyecto/mochituscamina.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mochituscamina : MonoBehaviour {
+
+    public float v;
+    public int t;
+    private Animator ani;
+
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        t++;
+        ani.SetBool("caminar", true);
+        
+        if(t <100)
+        {
+            transform.Translate(v, 0, 0);
+        }
+        if (t > 100 && t < 200)
+        {
+            transform.Translate(-v, 0, 0);
+            t = 0;
+        }
+    }
+}
