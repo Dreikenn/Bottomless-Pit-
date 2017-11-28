533c909a12b019bf0b452fbd703007950320617d
fedejavitaroco <31699354+fedejavitaroco@users.noreply.github.com>
2017-11-28 12:53:28 -0300

    intento de chechpoint (no se xq no funciona :()
    mañana igual muestro lo q hice sino apra q me digan xq no se activa y creo q es xq el codigo de morir

---
+++ Bottomless Pit/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simple player controller based on a Rigidbody
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody thisRigidbody;
+
+    void Start()
+    {
+        thisRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+      
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            thisRigidbody.transform.position = CheckPoint.GetActiveCheckPointPosition();
+        }
+    }
+}
