3fb381d0c498a557c32ef23908e87d8f6a3fc1a7
fedejavitaroco <31699354+fedejavitaroco@users.noreply.github.com>
2017-12-07 16:41:14 -0300

    checkpoints
    ahi estan los checkpoints espero q se suba xq ahora me voy al trabajo. cualquier cosa me dicen

---
+++ Bottomless Pit/Assets/checkfalso.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkfalso : MonoBehaviour {
+	public GameObject tp;
+
+
+
+	public void OnTriggerEnter(Collider hit)
+	{
+
+		hit.transform.position = tp.transform.position;
+
+
+	}
+}
